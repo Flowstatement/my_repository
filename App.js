9da24b3051da7686954c13bd004899461f31fa85
Flowstatement <111563391+Flowstatement@users.noreply.github.com>
2022-09-15 17:35:57 +0900

    useEffect
    About useEffect that I learned.
    useEffect is the most useful hooks in the React.

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import Button from "./Button.js";
 import Electric_fan from "./Electric.js";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render" + counter);
+  // console.log("render " + counter);
+  // const this_is_sparta = () => {console.log("I live only once");}
+  // useEffect(this_is_sparta ,[]);
+  useEffect(() => console.log("I live only once"), []);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => {
+    // console.log(event.target.value);
+    setKeyword(event.target.value);
+  };
+  // useEffect(() => console.log(keyword), [keyword]);
+  // useEffect(() => console.log("render " + counter), [counter]);
+  useEffect(
+    () => console.log("I run when 'counter' and 'keyword' changes."),
+    [counter, keyword]
+  );
+
   return (
     <div>
+      <input
+        onChange={onChange}
+        type="text"
+        placeholder="search anything"
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>+1</button>
     </div>
