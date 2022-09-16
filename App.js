9b356c4dadac00a3d081eb6e93240a3ade005074
Flowstatement <111563391+Flowstatement@users.noreply.github.com>
2022-09-16 17:28:59 +0900

    make Todolist (step 01)
    I start to make a todolist on React.
    I use array useState and array copy.
    and also use JSX for showing plan array.length.
    Thank you.

--- App.js
+++ App.js
@@ -1,36 +1,32 @@
-import Button from "./Button.js";
-import Electric_fan from "./Electric.js";
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  // console.log("render " + counter);
-  // const this_is_sparta = () => {console.log("I live only once");}
-  // useEffect(this_is_sparta ,[]);
-  useEffect(() => console.log("I live only once"), []);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => {
-    // console.log(event.target.value);
-    setKeyword(event.target.value);
-  };
-  // useEffect(() => console.log(keyword), [keyword]);
-  // useEffect(() => console.log("render " + counter), [counter]);
-  useEffect(
-    () => console.log("I run when 'counter' and 'keyword' changes."),
-    [counter, keyword]
-  );
+  const [todo_value, setTodo_value] = useState("");
+  const write_something = (event) => setTodo_value(event.target.value);
 
+  const [plan_array, setPlan_array] = useState([]);
+  const add_plan = (e) => {
+    e.preventDefault();
+    console.log(todo_value);
+    if (todo_value === "") return;
+    else {
+      setPlan_array((currentArray) => [todo_value, ...currentArray]);
+      setTodo_value("");
+    }
+    console.log(plan_array);
+  };
   return (
     <div>
-      <input
-        onChange={onChange}
-        type="text"
-        placeholder="search anything"
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>+1</button>
+      <h1>Current Array: ({plan_array.length})</h1>
+      <form onSubmit={add_plan}>
+        <input
+          onChange={write_something}
+          value={todo_value}
+          type="text"
+          placeholder="계획을 적으세요."
+        />
+        <button>입력</button>
+      </form>
     </div>
   );
 }
